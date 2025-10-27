a38f953e768be3b50f13599553fd8c38aad13230
Natalya <nidnatasha@gmail.com>
2025-10-27 09:37:09 +0000

    Added calculation of level counts and displaying them

--- task_3.py
+++ task_3.py
@@ -1,7 +1,10 @@
+from collections import Counter
+import sys
 
 
 def parse_log_line(line: str) -> dict:
     words_lst = line.split(" ")
+    # Parse the line and form a dictionary:
     log_dict = dict()
     log_dict["date"] = words_lst[0]
     log_dict["time"] = words_lst[1]
@@ -13,39 +16,61 @@ def parse_log_line(line: str) -> dict:
 
 def load_logs(file_path: str) -> list:
     file_lst = []
-    
-    with open(file_path, "r") as f:
-        
-        while True:
-            line = f.readline()
-            if not line:
-                break
+    try:
+        # Read the file and parse lines. Get list of dictionaries:
+        with open(file_path, "r") as f:
             
-            file_lst.append(parse_log_line(line))
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                file_lst.append(parse_log_line(line))
     
-    return file_lst
+    except FileNotFoundError:
+        # Handle the case where the file is not found
+        print("Error: The specified file was not found.")  
+    
+    else:
+        return file_lst
     
 def filter_logs_by_level(logs: list, level: str) -> list:
     filtered_list = filter(lambda item: item['level'] == level, logs)
     filtered_list = list(filtered_list)
     return filtered_list
 
+def count_logs_by_level(logs: list) -> dict:
+    levels_list = []
+    
+    # Fill the empty list with levels
+    for item in logs:
+        levels_list.append(item["level"])
 
-list_file = load_logs("log.txt")
-filtered_lst = filter_logs_by_level(list_file, "ERROR")
-# print(list_file)
-print(filtered_lst)
+    return Counter(levels_list)
+
+def display_log_counts(counts: dict):
+    print("Рівень логування | Кількість")
+    for key, value in counts.items():
+        print(f"{key:<17}| {value:<9}")
 
 
+if __name__ == "__main__":
+    list_file = load_logs("log.txt")
+    levels_count = count_logs_by_level(list_file)
 
-# def count_logs_by_level(logs: list) -> dict:
-#     # проходить по всім записам і підраховує кількість записів для кожного рівня логування
+    if len(sys.argv) > 1:
+        first_argument = sys.argv[1].upper()
+        
+        # Print error levels and their counts:
+        display_log_counts(levels_count)
+        
+        print(f"\nДеталі логів для рівня \'{first_argument}\':")
+        
+        # Filter logs by level entered from consol:
+        filtered_lst = filter_logs_by_level(list_file, first_argument)
 
-# def display_log_counts(counts: dict):
-#     # Вивід результатів 
-#     # форматує та виводить результати підрахунку в читабельній формі
-
-# види помилок: відсутність файлу, помилки при його читанні, неправильний формат лог-файлу
-
-# При розробці обов'язково було використано один з елементів функціонального програмування: 
-# лямбда-функція, списковий вираз, функція filter, тощо
+        # Print the filtered logs by level:
+        for item in filtered_lst:
+            print(" ".join(list(item.values())).rstrip("\n"))
+    else:
+        # If the user doesn't enter error level, print ingo about all levels and their counts:
+        display_log_counts(levels_count)
